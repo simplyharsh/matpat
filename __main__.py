525c7d8895bac126f8eec43f1cf6bf48f053237a
Harsh Kohli <harsh@simplyharsh.com>
2016-03-10 00:00:33 +0530

    removed redundant files

--- __main__.py
+++
@@ -1,6 +0,0 @@
-from matrix import Matrix
-
-matrix = Matrix(2, 2)
-
-count, summary_counts = matrix.count_patterns()
-print "total_patterns: ", count, summary_counts
--- constants.py
+++
@@ -1,6 +0,0 @@
-"""
-Constants used in the package
-"""
-
-MAX_EDGE_REUSE = 1
-MIN_NODES = 4
